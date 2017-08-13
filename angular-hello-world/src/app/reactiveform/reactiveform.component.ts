9fff0a4641a26b0f31bd3cdfcd804d124a98dfe0
ashokkumaresan <ashok1989.stone@gmail.com>
2017-08-13 18:33:25 +0400

    Update

--- angular-hello-world/src/app/reactiveform/reactiveform.component.ts
+++ angular-hello-world/src/app/reactiveform/reactiveform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { FormGroup,FormControl,Validators } from '@angular/forms';
 
 @Component({
@@ -14,17 +15,42 @@ signupform:FormGroup;
   ngOnInit() {
   	this.signupform=new FormGroup({
   		'userData':new FormGroup({
-  		'username':new FormControl(null,Validators.required),
-  		'department':new FormControl(null,Validators.required)
+  		'username':new FormControl("",[Validators.required,this.limitLength.bind(this)]),
+  		'department':new FormControl("",[Validators.required],this.checkDepart)
   		}),
   		'experiance':new FormControl("three",Validators.required),
   		'answer':new FormControl("Yes",Validators.required),
-  		'reply':new FormControl(null,Validators.required)
+  		'reply':new FormControl("sss")
   	});
+
+    this.signupform.statusChanges.subscribe(
+      (value)=>{
+        console.log(value);
+      }
+    )
   }
 
   onSubmit(){
   	console.log(this.signupform);
+  }
+
+  limitLength(control:FormControl):{[s:string]:boolean} {
+    if(control.value.length>10)
+      return{'Invalid length':true};
+    else
+      return null;
+  }
+
+  checkDepart(control:FormControl):Promise<any> | Observable<any>{
+    const promise=new Promise<any>((resolve,reject)=>{
+      setTimeout(()=>{
+        if(control.value=="BI")
+        resolve({"InvalidDepartment":true});
+        else
+        resolve(null);
+      },1500)
+    });
+    return promise;
   }
 
 }
